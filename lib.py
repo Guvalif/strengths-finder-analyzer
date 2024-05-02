167124d47adf49f1c4a33f2e51057e358d8a412a
Kazuyuki-Takase <kazuyuki_takase@dwango.co.jp>
2024-05-02 12:50:28 +0900

    Fix rate limit bug

--- lib.py
+++ lib.py
@@ -52,35 +52,37 @@ Theme = Literal[
 ThemesTable = dict[str, list[Theme]]
 
 
-def histogram(table: ThemesTable, rate=5) -> tuple[Counter[Theme], Counter[Theme]]:
+def histogram(table: ThemesTable, rate: int = 5) -> tuple[dict[Theme, int], dict[Theme, int]]:
+    empty = { theme: 0 for theme in Theme.__args__ } # type: ignore
+
     s_hist = Counter(chain.from_iterable(map(lambda xs: xs[:rate], table.values())))
     w_hist = Counter(chain.from_iterable(map(lambda xs: xs[-rate:], table.values())))
 
-    return (s_hist, w_hist)
+    return ({ **empty, **s_hist }, { **empty, **w_hist })
 
 
 def _jaccard(Xi: set, Xj: set) -> float:
     return len(Xi & Xj) / len(Xi | Xj)
 
 
-def _distance(Xi: list[Theme], Xj: list[Theme], rate=5) -> float:
+def _distance(Xi: list[Theme], Xj: list[Theme], rate: int) -> float:
     s_jaccard = _jaccard(set(Xi[:rate]), set(Xj[:rate]))
     w_jaccard = _jaccard(set(Xi[-rate:]), set(Xj[-rate:]))
 
     return 1 - (s_jaccard + w_jaccard) / 2
 
 
-def distance_gen(table: ThemesTable) -> Generator[tuple[str, str, float], None, None]:
+def distance_gen(table: ThemesTable, rate: int = 5) -> Generator[tuple[str, str, float], None, None]:
     for (name_i, Xi), (name_j, Xj) in combinations(table.items(), 2):
-        yield (name_i, name_j, _distance(Xi, Xj))
+        yield (name_i, name_j, _distance(Xi, Xj, rate))
 
 
-def _skipped_union(name_i: str, table: ThemesTable) -> set[Theme]:
-    skipped = (set(Xj) for name_j, Xj in table.items() if name_j != name_i)
+def _skipped_union(name_i: str, table: ThemesTable, rate: int) -> set[Theme]:
+    skipped = (set(Xj[:rate]) for name_j, Xj in table.items() if name_j != name_i)
 
     return reduce(lambda x, y: x | y, skipped)
 
 
-def specific_gen(table: ThemesTable) -> Generator[tuple[str, set[Theme]], None, None]:
+def specific_gen(table: ThemesTable, rate: int = 5) -> Generator[tuple[str, set[Theme]], None, None]:
     for name_i, Xi in table.items():
-        yield (name_i, set(Xi) - _skipped_union(name_i, table))
+        yield (name_i, set(Xi[:rate]) - _skipped_union(name_i, table, rate))
